d29cd45d7a313224c050b368a2d8be2746edb11d
praniil <pparajuli15@gmail.com>
2024-02-03 16:57:42 +0545

    feat: bubble sort completed and showed graphically on the window

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -7,6 +7,32 @@
 
 void bubble_sort(sf::RenderWindow &window, std::vector<int> &arr)
 {
+    bool swapped;
+    do
+    {
+        swapped = false;
+        for (int i = 0; i < arr.size(); i++)
+        {
+
+            if (arr[i] > arr[i + 1])
+            {
+                int temp;
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+                window.clear();
+                for (int j = 0; j < arr.size(); ++j)
+                {
+                    sf::RectangleShape rect(sf::Vector2f(5.f, arr[j]));
+                    rect.setFillColor(sf::Color::Cyan);
+                    rect.setPosition(j * 6.f, window.getSize().y - arr[j]);
+                    window.draw(rect);
+                }
+                window.display();
+            }
+        }
+    } while (swapped);
 }
 
 int BubbleSort()
@@ -14,7 +40,7 @@ int BubbleSort()
     sf::RenderWindow window(sf::VideoMode(800, 600), "Bubble sort algorithm");
 
     // random array generated
-    std::vector<int> arr(800);
+    std::vector<int> arr(400);
     std::iota(arr.begin(), arr.end(), 1);
     std::shuffle(arr.begin(), arr.end(), std::default_random_engine(std::chrono::system_clock::now().time_since_epoch().count()));
 
